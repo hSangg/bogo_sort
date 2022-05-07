6b3d655be80f52d82de43ed27e48be231ec33514
Sang <hoai.sang050@gmail.com>
2022-05-07 17:28:12 +0700

    second commit

---
+++ bogo_sort.js
@@ -0,0 +1,24 @@
+const isSorted = (a, n) => {
+  while (--n >= 1) if (a[n] < a[n - 1]) return false;
+
+  return true;
+};
+
+const suffle = (a) => {
+  for (let i = 0; i < a.length; i++) {
+    const randomIndex = Math.floor(Math.random() * a.length);
+
+    let temp = a[i];
+    a[i] = a[randomIndex];
+    a[randomIndex] = temp;
+  }
+};
+
+const bogoSort = (a, n) => {
+  while (!isSorted(a, n)) suffle(a);
+};
+
+const a = [1, 6, 3, 5, 6, 3, 5, 3, 2, 4];
+bogoSort(a, a.length);
+
+a.forEach((x) => console.log(x));
